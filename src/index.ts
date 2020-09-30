854310b549ec4485dc71f34a3bccc2b924934ce9
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-01 01:32:54 +0200

    Query

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,12 @@ import * as http from 'http';
 import * as url from 'url';
 
 const server = http.createServer((req, res) => {
-    const path = url
-        .parse(req.url, true)
-        .pathname
-        .replace(/^\/+|\/+$/g, '');
+    const parsedUrl = url.parse(req.url, true);
+    const endpoint = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
+    const query = parsedUrl.query;
     const method = req.method;
-    res.end(path);
-    console.log(method + ' request on: ' + path);
+    res.end(endpoint);
+    console.log(method + ' request on: ' + endpoint + ' with: ', query);
 });
 
 server.listen(3000, () => console.log('Listening on port 3000'));
