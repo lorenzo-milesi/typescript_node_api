f02f5838c0baebd899c25f2466d6cbfc8ddcdd5c
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-01 01:24:45 +0200

    Parse Request Path

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,15 @@
 
 // Dependencies
 import * as http from 'http';
+import * as url from 'url';
 
 const server = http.createServer((req, res) => {
-    res.end('Hi !');
+    const path = url
+        .parse(req.url, true)
+        .pathname
+        .replace(/^\/+|\/+$/g, '');
+    res.end(path);
+    console.log('New request on: ' + path)
 });
 
-server.listen(3000, () => console.log('Listening on port 3000'))
+server.listen(3000, () => console.log('Listening on port 3000'));
